4630f045768f51108cf4162f5c561b28ad311daa
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-05 01:59:55 +0800

    init native macOS app for FSKit

---
+++ apps/desktop/desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  desktop
+//
+//  Created by Oscar Beaumont on 5/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/desktop/desktop/desktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  desktopApp.swift
+//  desktop
+//
+//  Created by Oscar Beaumont on 5/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct desktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
