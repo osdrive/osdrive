03f469b8475b1bf2e8f7f16cf3577a6bbc29cf82
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-07 00:02:31 +0800

    rename `DB` binding + fix it

--- apps/web/src/lib/db.ts
+++ apps/web/src/lib/db.ts
@@ -3,5 +3,5 @@ import { drizzle } from "drizzle-orm/d1";
 import { authSchema } from "~/lib/db/schema/auth";
 
 export function getDb() {
-  return drizzle(env.AUTH_DB, { schema: authSchema });
+  return drizzle(env.DB, { schema: authSchema });
 }
