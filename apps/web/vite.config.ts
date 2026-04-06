b01b2c8e7737cb93f1ab51119e0c812c6e65bea4
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-07 01:24:41 +0800

    remove existing stuff + tsconfig paths

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import { solidStart } from "@solidjs/start/config";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     solidStart(),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
+    // TODO: Remove this once Vite is updated to the latest as I think it has this built in?
+    tsconfigPaths(),
   ]
 });
