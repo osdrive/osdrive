c254801c7465fa03101fac8e75740513b14d5c40
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-07 02:28:23 +0800

    more refinements

---
+++ apps/web/src/routes/dashboard/index.tsx
@@ -0,0 +1,8 @@
+// TODO:
+//  - Dashboard layout
+//  - Tenant switching and creation
+//  - Auth (logout + separate login flow)
+//  - Tenant settings w/ billing, account management
+//  - Contact/feedback button
+
+export default function Page() {}
